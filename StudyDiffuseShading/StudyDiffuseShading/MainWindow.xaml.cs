02e6ca3efa6127de458ea694e4341b6fb110c334
Nakata Nobuu <nobu.nakata@gmail.com>
2011-05-27 13:26:32 +0900

    add project DiffuseShading

---
+++ StudyDiffuseShading/StudyDiffuseShading/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using StudyDiffuseShading.ViewModel;
+
+namespace StudyDiffuseShading
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+        public const string KEY_MODEL = "MainWindowVMDataSource";
+
+
+        private MainWindowVM model;
+
+
+		public MainWindow()
+		{
+			this.InitializeComponent();
+
+            this.model = (MainWindowVM) FindResource(KEY_MODEL);
+		}
+
+		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+            model.render((int) imageRendered.Width, (int) imageRendered.Height);
+		}
+	}
+}
---
+++ StudyDiffuseShading/StudyDiffuseShading/ViewModel/MainWindowVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace StudyDiffuseShading.ViewModel {
+    public class MainWindowVM : DependencyObject {
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register(
+            "Image", typeof(BitmapSource), typeof(MainWindowVM));
+
+
+        public MainWindowVM() {
+
+        }
+
+
+        public BitmapSource Image {
+            get {
+                return (BitmapSource) GetValue(ImageProperty);
+            }
+            set {
+                SetValue(ImageProperty, value);
+            }
+        }
+
+        public void render(int width, int height) {
+            
+        }
+    }
+}
